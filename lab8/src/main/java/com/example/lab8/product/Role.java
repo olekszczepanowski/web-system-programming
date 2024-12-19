fd29241ca0664d3bcbda9e29fbaa56c8443d64e5
olekszczepanowski <olekszczepanowski04@wp.pl>
2024-12-19 15:54:28 +0100

    fix: delete unused directories

--- lab8/src/main/java/com/example/lab8/product/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.lab8.product;
-
-public enum Role {
-    CUSTOMER,
-    ADMIN
-}
--- lab8/src/main/java/com/example/lab8/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.lab8.repository;
-
-import com.example.lab8.product.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface CategoryRepository extends JpaRepository<Category,Long> {
-
-}
